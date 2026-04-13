70131bd978d67b949f47c130fc9dd9ac6535cbac
Adam@Ecodia <adam.ecodia@gmail.com>
2026-04-13 00:49:59 -0400

    add run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Load Python virtual environment and start the server in background
 source venv/bin/activate
-python3 -m study_bible_mcp.server --transport stdio &
+python -m study_bible_mcp.server --transport stdio 
 
-# Save the PID to a file
-echo $! > server.pid
-
-echo "Server started with PID $(cat server.pid)"
